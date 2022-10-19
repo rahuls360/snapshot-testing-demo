4d87c3734ae4dbb83ed73a51c165d5ed6653e19b
Rahul Makhija <rahuls360@gmail.com>
2022-10-19 13:29:56 +0530

    add react-test-renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('snapshot testing', () => {
+  it('should render App correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
