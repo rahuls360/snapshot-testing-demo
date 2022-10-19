6587915e859d0e219998a3676885004831bb35a7
Rahul Makhija <rahuls360@gmail.com>
2022-10-19 13:29:56 +0530

    update snapshot of only App component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -25,7 +22,7 @@ function App() {
 export function Child() {
   return (
     <div className="Child">
-     I am the first child
+     I am the child
     </div>
   );
 }
